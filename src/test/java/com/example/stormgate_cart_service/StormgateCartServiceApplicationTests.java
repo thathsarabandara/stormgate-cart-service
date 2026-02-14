745c5c31d2201e9b175ecf61a869cfa467725629
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-14 08:24:58 +0530

    Add initial test class for StormgateCartService application context

---
+++ src/test/java/com/example/stormgate_cart_service/StormgateCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stormgate_cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormgateCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
