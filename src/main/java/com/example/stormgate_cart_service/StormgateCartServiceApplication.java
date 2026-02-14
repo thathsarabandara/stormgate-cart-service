532aa856db0b51f069b7654726cedfc652206cb3
thathsarabandara <thathsaraarumapperuma@gmail.com>
2026-02-14 08:22:57 +0530

    Add StormgateCartServiceApplication class for application entry point

---
+++ src/main/java/com/example/stormgate_cart_service/StormgateCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stormgate_cart_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormgateCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormgateCartServiceApplication.class, args);
+	}
+
+}
